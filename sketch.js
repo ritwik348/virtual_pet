37ad6f95ee5ae42de88b35f22760040a14cf8781
ritwik348 <78126537+ritwik348@users.noreply.github.com>
2021-04-22 22:00:56 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var dog
+var dogHappy
+var foodS
+var foodStock
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
